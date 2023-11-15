d6e218ccb1acdc739dc88cd0febb2608d6149dd6
RikuHonkamaki <81312875+RikuHonkamaki@users.noreply.github.com>
2023-11-15 10:51:56 +0200

    Hit sound added

--- Assets/Scripts/BodyPartHitDetection.cs
+++ Assets/Scripts/BodyPartHitDetection.cs
@@ -9,6 +9,17 @@ public class BodyPartHitDetection : MonoBehaviour
     public float cooldownTime = 0.2f; // Aika sekunneissa ennen kuin uusi osuma voidaan ottaa vastaan.
     private bool canHit = true;
 
+    public AudioSource hitSound; // Reference to the AudioSource for the hit sound
+
+    private void Start()
+    {
+        // Ensure that the AudioSource is set up in the Unity Inspector
+        if (hitSound == null)
+        {
+            Debug.LogError("Hit sound AudioSource is not assigned!");
+        }
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if (canHit && OnBodyPartHit != null)
@@ -17,12 +28,20 @@ public class BodyPartHitDetection : MonoBehaviour
             if (rb != null)
             {
                 OnBodyPartHit(gameObject.tag, rb);
+                PlayHitSound(); // Play the hit sound
             }
             canHit = false;
             Invoke("ResetHit", cooldownTime);
         }
     }
 
+    void PlayHitSound()
+    {
+        if (hitSound != null)
+        {
+            hitSound.Play();
+        }
+    }
 
     // Metodi, joka sallii osumien vastaanoton.
     void ResetHit()
