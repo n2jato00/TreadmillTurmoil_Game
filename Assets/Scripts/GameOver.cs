f29b06d202c1c0cd2bcdfa211e3594bdc477baaa
RikuHonkamaki <81312875+RikuHonkamaki@users.noreply.github.com>
2023-11-15 10:11:02 +0200

    More audio stuff

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -7,33 +7,36 @@ using System.Collections.Generic;
 public class GameOver : MonoBehaviour
 {
     public GameObject gameOverPanel;
+    public AudioSource backgroundMusic; // Reference to your background music AudioSource
 
-  
     void Start()
     {
         gameOverPanel.SetActive(false);
         Time.timeScale = 1;
-
     }
 
     public void EndGame()
     {
         Time.timeScale = 0;
         gameOverPanel.SetActive(true);
+
+        // Stop the background music when the game is over
+        if (backgroundMusic != null)
+        {
+            backgroundMusic.Stop();
+        }
     }
 
     public void RestartGame()
     {
-        // Lataa uudelleenk‰ynnistyksen yhteydess‰ "Game1" -kohtaus.
+        // Reload the "Game1" scene when restarting
         Time.timeScale = 1;
         SceneManager.LoadScene("Game1");
-       
-
     }
 
     public void GoMainMenu()
     {
-        // Lataa p‰‰valikko "StartScreen" -kohtaus.
+        // Load the main menu "StartScreen" scene
         SceneManager.LoadScene("StartScreen");
     }
 }
