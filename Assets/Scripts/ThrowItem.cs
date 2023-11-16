14b955e7bf3b852e6201ee21185c865dd0d3ba0f
n2jato00 <n2jato00@students.oamk.fi>
2023-11-16 10:39:51 +0200

    change ThrowItem

--- Assets/Scripts/ThrowItem.cs
+++ Assets/Scripts/ThrowItem.cs
@@ -1,28 +1,22 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
 
 public class ThrowItem : MonoBehaviour
 {
     [Header("Throw Settings")]
-    [SerializeField]
-    private float maxThrowForce = 50f;
-    [SerializeField]
-    private float minSwipeThreshold = 10f; // Minimum swipe distance to count as a throw
-    
-    private float maxSwipeDistance = 200f; // Maximum swipe distance to count for max force
-
+    [SerializeField] private float maxThrowForce = 50f;
+    [SerializeField] private float minSwipeThreshold = 10f;
+    private float maxSwipeDistance = 200f;
 
     [Header("Drag Settings")]
-    [SerializeField]
-    private float followSpeed = 5f;
+    [SerializeField] private float followSpeed = 5f;
 
     private Vector2 startSwipePos;
     private Vector2 endSwipePos;
     private bool isDragging = false;
+    private bool isThrown = false; // Lis‰‰ muuttuja isThrown
+
     public static event Action OnBallThrown;
 
     private Rigidbody rb;
@@ -46,24 +40,23 @@ public class ThrowItem : MonoBehaviour
 
     private void HandleInput()
     {
-        // If the game is paused, don't process touch for throwing
-        if (Time.timeScale == 0 || !PauseManager.canThrow) // Tarkista canThrow-tila
+        if (isThrown) // Lis‰‰ tarkistus, jos esine on heitetty
             return;
 
-        // Check if there are any touches
+        if (Time.timeScale == 0 || !PauseManager.canThrow)
+            return;
+
         for (int i = 0; i < Input.touchCount; i++)
         {
             Touch touch = Input.GetTouch(i);
 
             if (touch.phase == TouchPhase.Began)
             {
-                // Check if touch started on a UI element
                 if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
                 {
-                    return; // Ignore this touch
+                    return;
                 }
 
-                // Handle the touch for throwing
                 startSwipePos = touch.position;
                 isDragging = true;
                 rb.useGravity = false;
@@ -72,21 +65,23 @@ public class ThrowItem : MonoBehaviour
             {
                 endSwipePos = touch.position;
                 isDragging = false;
-                
 
                 if (Vector2.Distance(startSwipePos, endSwipePos) > minSwipeThreshold)
                 {
                     rb.useGravity = true;
                     ThrowBall();
                     PauseManager.canThrow = false;
+                    isThrown = true; // Merkitse, ett‰ esine on heitetty
                 }
             }
         }
     }
 
-
     private void HandleDrag()
     {
+        if (isThrown) // Lis‰‰ tarkistus, jos esine on heitetty
+            return;
+
         if (isDragging)
         {
             float distanceToCamera = Vector3.Distance(transform.position, Camera.main.transform.position);
@@ -94,7 +89,6 @@ public class ThrowItem : MonoBehaviour
             Vector3 touchPosInWorld = Camera.main.ScreenToWorldPoint(touchPosOnScreen);
             touchPosInWorld.z = transform.position.z;
 
-            // Check if the ball is close enough to the touch position
             if (Vector3.Distance(transform.position, touchPosInWorld) < 0.05f)
             {
                 rb.MovePosition(touchPosInWorld);
@@ -106,8 +100,6 @@ public class ThrowItem : MonoBehaviour
         }
     }
 
-
-
     private void ThrowBall()
     {
         Vector2 swipeDir = endSwipePos - startSwipePos;
@@ -115,15 +107,11 @@ public class ThrowItem : MonoBehaviour
         float normalizedSwipeLength = Mathf.Clamp01(swipeDir.magnitude / maxSwipeDistance);
         float throwForce = normalizedSwipeLength * maxThrowForce;
 
-        // Convert 2D swipe direction into 3D force direction
         Vector3 forceDirection = new Vector3(swipeDir.x, swipeDir.y, 0).normalized;
-
-        // Rotate this force direction from screen space into world space.
         forceDirection = Camera.main.transform.TransformDirection(forceDirection);
-        forceDirection.z = 1;  // Ensures forward motion along the z-axis.
+        forceDirection.z = 1;
 
         rb.AddForce(forceDirection * throwForce, ForceMode.Impulse);
         OnBallThrown?.Invoke();
     }
-
 }
