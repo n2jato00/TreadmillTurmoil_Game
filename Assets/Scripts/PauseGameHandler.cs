f5d0f9ac5f8108b16fe625cd25bb2fb410bec00e
RikuHonkamaki <81312875+RikuHonkamaki@users.noreply.github.com>
2023-11-16 11:31:44 +0200

    Äänet korjattu

--- Assets/Scripts/PauseGameHandler.cs
+++ Assets/Scripts/PauseGameHandler.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseGameHandler : MonoBehaviour
+
 {
+
+    public AudioSource backgroundMusic; // Reference to your background music AudioSource
+
     public void PauseGame()
     {
         Time.timeScale = 0;
         PauseManager.canThrow = false; // Stop throws when Pause is activated
+
+        if (backgroundMusic != null)
+        {
+            backgroundMusic.Stop();
+        }
     }
     public void ResumeGame()
     {
@@ -17,8 +26,10 @@ public class PauseGameHandler : MonoBehaviour
     private IEnumerator ResumeGameWithDelay()
     {
         Time.timeScale = 1;
+        backgroundMusic.Play();
         yield return new WaitForSeconds(0.5f);
         PauseManager.canThrow = true;
+       
     }
 }
 
