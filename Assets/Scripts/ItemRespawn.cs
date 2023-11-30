56c9ec7dd0911d9458d37a2300d1dde5f98c61ee
n2jato00 <n2jato00@students.oamk.fi>
2023-11-30 11:46:00 +0200

    update time

--- Assets/Scripts/ItemRespawn.cs
+++ Assets/Scripts/ItemRespawn.cs
@@ -4,7 +4,7 @@ public class ItemRespawn : MonoBehaviour
 {
     public GameObject[] itemPrefabs; // Set this array in the Unity Editor.
     public float itemLifetime = 5f;
-    public float createNewItemTime = 0.5f;
+    public float createNewItemTime = 0.25f;
     public float distanceInFrontOfCamera = 1.6f; // How far in front of the camera the object appears.
     public float heightAboveCamera = 0f; // How high above the camera the object appears.
     public float lateralOffsetFromCamera = 0.1f; // How far to the side from the camera the object appears.
